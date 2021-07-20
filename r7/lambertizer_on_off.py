adf336a31c333dd3b7265d4d3966bdbd2e09653e
Patcha <patchasaheb@gmail.com>
2021-07-20 11:49:05 -0400

    Updated for Python3 / 3DE4 R7.0

--- r7/lambertizer_on_off.py
+++ r7/lambertizer_on_off.py
@@ -1,8 +1,8 @@
-# 3DE4.script.name:	Lamb on/off
-# 3DE4.script.version:	v1.0
-# 3DE4.script.gui.button:	Lineup Controls::Lamb on/off, align-bottom-left, 80, 20
-# 3DE4.script.gui.button:	Orientation Controls::Lamb on/off, align-bottom-left, 70, 20
-# 3DE4.script.comment:toggles lambertizer on/off.
+# 3DE4.script.name: Lamb on/off
+# 3DE4.script.version: v1.0
+# 3DE4.script.gui.button: Lineup Controls::Lamb on/off, align-bottom-left, 80, 20
+# 3DE4.script.gui.button: Orientation Controls::Lamb on/off, align-bottom-left, 70, 20
+# 3DE4.script.comment: toggles lambertizer on/off.
 
 # July 20 2021, Montreal
 # Patcha Saheb(patchasaheb@gmail.com)
@@ -10,9 +10,9 @@
 pg = tde4.getCurrentPGroup()
 cam = tde4.getCurrentCamera()
 frame = tde4.getCurrentFrame(cam)
-mlist = tde4.get3DModelList(pg,1)
-if len(mlist) > 0:
-	for model in mlist:	
+model_list = tde4.get3DModelList(pg,1)
+if len(model_list) > 0:
+	for model in model_list:	
 		per_vertex = tde4.get3DModelPerVertexColorsFlag(pg,model)
 		if per_vertex == 1:
 			tde4.set3DModelPerVertexColorsFlag(pg,model,0)
